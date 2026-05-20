2a3026179cbd7a311e4277124619c6085523144a
random986 <ronodesmond7@gmail.com>
2026-05-20 16:14:46 +0300

    Restore affiliate_token to OAuth redirect URL for production deployment

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -294,7 +294,7 @@ export default function Settings() {
                 <button
                   onClick={() => {
                     const redirectUri = encodeURIComponent(window.location.origin);
-                    window.location.href = `https://oauth.deriv.com/oauth2/authorize?app_id=${appId}&redirect_uri=${redirectUri}`;
+                    window.location.href = `https://oauth.deriv.com/oauth2/authorize?app_id=${appId}&redirect_uri=${redirectUri}&affiliate_token=33h51PQlu5tsWflEmmoxW`;
                   }}
                   style={{
                     width: '100%',
