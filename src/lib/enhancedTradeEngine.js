50f8b66d8197f559176c13c1e42c77c7f7194ec6
random986 <ronodesmond7@gmail.com>
2026-05-21 04:36:45 +0300

    Revert quad-firing to dual-firing (two concurrent trades) for EO/OU winning strategies

--- src/lib/enhancedTradeEngine.js
+++ src/lib/enhancedTradeEngine.js
@@ -870,7 +870,7 @@ class EnhancedTradeEngine {
       this.updateStatus('Executing');
       this._placeTrade('SINGLE', chosenDirection, this.config.baseStake);
     } else {
-      const dirs = ['EVEN', 'ODD', 'OVER5', 'UNDER5'];
+      const dirs = this.strategy === 'OU_WINNING' ? ['OVER5', 'UNDER5'] : ['EVEN', 'ODD'];
       let tradesPlaced = 0;
       this.updateStatus('Executing');
       dirs.forEach(dir => {
